b7e7b5ec274a8c9788813be2cdfce33b399aad36
Chrisdocs <chrisa.wolfe@gmail.com>
2021-07-26 17:12:37 -0500

    tests

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -14,38 +14,29 @@ const Nav = () => {
       description: "Fields, farmhouses, waterfalls, and the beauty of nature",
     },
   ];
-  
+
   function categorySelected(name) {
-    console.log(`${name} clicked`)
+    console.log(`${name} clicked`);
   }
 
   return (
     <header>
       <h2>
-        <a href="/">
-          <span role="img" aria-label="camera">
-            {" "}
-            📸
-          </span>{" "}
-          Oh Snap!
+        <a data-testid="link" href="/">
+          <span role="img" aria-label="camera"> 📸</span>Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about">About me</a>
+            <a data-testid="about" href="#about">About me</a>
           </li>
           <li>
             <span>Contact</span>
           </li>
           {categories.map((category) => (
-            <li 
-            className="mx-1" 
-            key={category.name}
-            >
-              <span onClick={ categorySelected } >
-                  {category.name}
-                  </span>
+            <li className="mx-1" key={category.name}>
+              <span onClick={categorySelected}>{category.name}</span>
             </li>
           ))}
         </ul>
